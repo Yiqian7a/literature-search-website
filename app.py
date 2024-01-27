a86940d7dd8d20b5de97125e03d2723d342bfc60
YqnIcedge6 <Yiqian7a@qq.com>
2024-01-27 11:25:33 +0800

    dev first

--- app.py
+++ app.py
@@ -1,27 +1,76 @@
-from flask import Flask, session, render_template, request, jsonify
+from flask import Flask, session, render_template, request, jsonify, json
+# from database import db
+import logging
+from flask_wtf import CSRFProtect
 
 # 创建flask实例对象
-app = Flask(__name__)
+app = Flask(__name__, template_folder=r".\templates")
+# app.config.from_object(db.DevelopmentConfig)
+# db.init_app(app)
+
+# 设置日志级别
+app.logger.setLevel(logging.INFO)
+
+app.config['SECRET_KEY'] = 'your_secret_key'  # 设置一个用于加密表单令牌的密钥
+csrf = CSRFProtect(app)
+
+# 判断当前用户是否在session中。由于flask要求命名空间映射唯一，所以用exec代替直接定义wrapper函数，结果是一样的
+def if_session(func):
+    func_name = func.__name__ + "_wrapper"
+    print(f"wrapping function: {func_name}")
+    exec("""
+def {name}(*args, **kwargs):
+    if "user_name" in session:
+        return func(*args, **kwargs)
+    else:
+        print("user not in session, locate to login")
+        return render_template("login_register.html")
+    return {name}
+""".format(name = func_name))
+    return eval(func_name)
 
 # 创建根路由
 @app.route('/')
+@if_session
 def home():
-    if 'user_name' not in session:
-        return render_template('login.html')
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/index')
+@app.route('/index', methods=["GET", "POST"])
+# @if_session
 def index():
-    pass
+    if request.method == "GET":
+        return render_template('index.html')
+    elif request.method == "POST":
+        pass
 
-@app.route('/login')
-def login():
-    pass
 
-@app.route('/register')
-def register():
-    pass
+@app.route('/login_register', methods=["GET", "POST"])
+def login_register():
+    if request.method == 'GET':
+        return render_template('login_register.html')
+    elif request.method == 'POST':
+        data = request.json
+        print(data)
+        if 'username' in data: # 为注册请求
+            return jsonify({'state': 200, "message": "注册成功"})
+        else: # 为登入请求
+
+            with open("./data/userdata.json", 'r') as f:
+                userdata = json.load(f)
+            if username in userdata and data['password'] == userdata[username]['password']:
+                # 创建session对象存储用户名，将用户名存储到 session 中
+                session['user_name'] = username[1:]
+                return jsonify({'state': 200, "message": "登录成功"})
+            elif username not in list(userdata):
+                return jsonify({'state': 501, 'message': '用户未注册'})
+            else:
+                return jsonify({'state': 502, 'message': '用户名或密码错误'})
+
+@app.route('/logout')
+def logout():
+    session.pop('user_name', None)
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
---
+++ try.py
@@ -0,0 +1,51 @@
+
+dic = {
+    "user":1
+}
+print("usr" in dic, "222")
+
+
+
+
+
+
+
+s="1"
+name="f1"
+exec("""def {}():
+    print('in func {}')
+    print('in func {}')
+print("out of func {}")""".format(name,name,name,name))
+# f1()
+def if_session(arg1):
+    print(arg1, "开始装饰函数")
+    if arg1 not in "123":
+        print(1,"进入if")
+        def inner(func):
+            def wrapper(*args, **kwargs):
+                print(2, "打包函数并传入参数")
+                return func(*args, **kwargs) # 返回函数func(*args,**kwargs)，不运行
+            return wrapper
+        print(3)
+        return inner
+    else:
+        print("no")
+
+# 等价于
+def ff(arg1, func, *args, **kwargs):
+    print(arg1)
+    print(1)
+    print(2)
+    print(3)
+    return func(arg1, *args, **kwargs)
+
+@if_session("4")
+def main():
+    print("good")
+
+@if_session("5")
+def main1():
+    print("bad")
+
+print(main)
+print(main1)
