56def181cbaac172de151acdd6ce3b01c7ad1328
YqnIcedge6 <Yiqian7a@qq.com>
2024-01-27 16:07:56 +0800

    mysql first succeed

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
 from datetime import timedelta
 import os
 
@@ -14,27 +16,49 @@ class BaseConfig:
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/literaturesearch?charset=utf8mb4"
+    # 缓存配置
+    CACHE_TYPE = "RedisCache"
+    CACHE_REDIS_HOST = "127.0.0.1"
+    CACHE_REDIS_PORT = 6379
+
+    AVATARS_SAVE_PATH = os.path.join(BaseConfig.UPLOAD_IMAGE_PATH, "avatars")
 
 
 def init_app(app):
     db = SQLAlchemy(app)
+    # 删除所有继承自db.Model的表
+    # db.drop_all()
 
+    global User
     class User(db.Model):
         __tablename__ = 'user'  # 设置表名, 表名默认为类名小写
         id = db.Column(db.Integer, primary_key=True) # 设置主键, 默认自增
-        username = db.Column(db.String(80), unique=True, nullable=False) # 设置字段名 和 唯一约束
-        email = db.Column(db.String(120), unique=True, nullable=False)
-
+        username = db.Column(db.String(80), nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False) # 设置字段名 和 唯一约束
+        password = db.Column(db.String(120), nullable=False)
+        # 执行print(实例)时返回的内容
         def __repr__(self):
             return '<User %r>' % self.username
 
-
     db.create_all()
+    return db
 
-    new_user = User(username='john', email='john@example.com')
-    db.session.add(new_user)
-    db.session.commit()
+def create_user(db_app, username:str, email:str, password:str):
+    try:
+        new_user = User(username=username, email=email, password=password)
+        db_app.session.add(new_user)
+        db_app.session.commit() # 提交更改
+    except IntegrityError:
+        db_app.session.rollback()
+        return False, "创建用户失败，此邮箱已被注册"
+    except Exception:
+        # 处理其他异常情况
+        db_app.session.rollback()
+        return False, "注册失败，请稍后再试。"
+    else: return True, "创建用户成功"
 
+def querry_user(db_app, email:str):
     all_users = User.query.all()
-    user = User.query.filter_by(username='john').first()
-    print(all_users,"\n",user)
+    user = User.query.filter_by(email=email).first()
+    print(all_users, "\n", user)
+
